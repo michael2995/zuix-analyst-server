5747abea64a3495d8dc8293dff16c9dd466843a2
michael2995 <msg@zigbang.com>
2021-03-14 14:21:38 +0900

    feat: increased json parse limit

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ import config from "../ormconfig.js"
 const setupServer = async(connection: Connection) => {
     const app = express()
     const port = 9999
-    app.use(express.urlencoded({extended: false}))
-    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+    app.use(express.json({ limit: "50mb" }))
 
     app.use("/", router)
 
